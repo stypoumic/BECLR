d250e0bf79ab818a87f4c373a9f87754df3bbe95
Stelios Poulakakis-Daktylidis <stypoumic@gmail.com>
2023-09-12 12:07:50 +0200

    Add support for FC-100 dataset

---
+++ dataset/FC100.py
@@ -0,0 +1,39 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+import pickle
+import csv
+
+
+class FC100(Dataset):
+    def __init__(self, data_path, partition='train', transform=None):
+        super(Dataset, self).__init__()
+        self.data_root = data_path
+        self.partition = partition
+        self.transform = transform
+
+        file_path = os.path.join(
+            self.data_root, 'FC100_{}.pickle'.format(self.partition))
+        self.imgs, self.labels = self._load_data(file_path)
+
+    def _load_data(self, file_path):
+        try:
+            with open(file_path, 'rb') as fo:
+                data = pickle.load(fo)
+        except:
+            with open(file_path, 'rb') as f:
+                u = pickle._Unpickler(f)
+                u.encoding = 'latin1'
+                data = u.load()
+        return data["data"], data["labels"]
+
+    def __getitem__(self, item):
+        img = self.imgs[item]
+        img = Image.fromarray(img)
+        if self.transform is not None:
+            img = self.transform(img)
+        target = self.labels[item]
+        return img, target
+
+    def __len__(self):
+        return len(self.labels)
